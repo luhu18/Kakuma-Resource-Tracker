42e88aa8850d8e9a87b788077022683df8835915
Lu Hu <husseinlucien7@gmail.com>
2025-04-14 12:40:52 +0300

    Chores: implement error massege and error hundler.

--- app.js
+++ app.js
@@ -13,8 +13,61 @@ let searchTerm = "";
 // initializing javascript to operate on the DOM
 
 function init() {
-    // Check if resources exist in local storage
     renderREsources();
     bindEvents();
     updateCounter();
+};[]
+
+//binding events to the DOM elements
+function bindEvents(){
+    form.addEventListener("submit", handleFormSubmit);
+    resourceList.addEventListener("click", handleDelete);
+    resourceList.addEventListener("dragover", hundleRspuraceClick);
+    resourceList.addEventListener("click", );
+    searchInput.addEventListener("input", handleSearch);
+    filterbutton.forEach((btn)=> {
+        button.addEventListener("click", handleFilter);
+    });
+
+    //handling events
+function handleFormSubmit(e) {
+    e.preventDefault();
 }
+    const formData = new FormData(form);
+    const resource = {
+        id: Date.now().toString(),
+        dateAdded: new Date().toLocaleDateString(),
+        name: formData.get("resourceName").trim(),
+        type: formData.get("resourceType"),};
+    
+if(validateForm(resource)){
+    addresource(resource);
+    form.reset();
+    clearError();
+}
+};[];
+//implemting form validation
+function validateForm(resource) {
+    if (!resource.name) {
+        showError("Resource name is required.");
+        return false;
+    }
+    if (!resource.type) {
+        showError("Resource type is required.");
+        return false;
+    }
+  if(!resource.loction){
+    showError("Resource name is required.");
+        return false;
+  }
+  return true
+};
+
+function showError(elementId,massege){
+    const errorElement= document.getElementById(elementId);
+    errorElement.textContent=message;
+}
+function clearError(elementId){
+    const errorElement= document.getElementById(elementId);
+    errorElement.textContent="";
+};
