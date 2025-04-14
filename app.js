d681ddcc0c3d6e60c216601dd90e64e8a3dbc6d6
Lu Hu <husseinlucien7@gmail.com>
2025-04-14 11:34:34 +0300

    Chore: grabbing DOM for munipilation.

---
+++ app.js
@@ -0,0 +1,7 @@
+//Dragging DOM elements for munipulation
+let form = document.getElementById("resourceForm");
+let resourceList = document.getElementById("resourceList");
+const counter = document.getElementById("counter");
+const searchInput = document.querySelector(".search-input");
+const filterbutton = document.querySelectorAll(".filter-btn");
+
