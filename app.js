0cf3df432c4a37ad39ec6599b8b5cc1566064ad5
Lu Hu <husseinlucien7@gmail.com>
2025-04-14 11:45:43 +0300

    Chores: implementing state handling.

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const counter = document.getElementById("counter");
 const searchInput = document.querySelector(".search-input");
 const filterbutton = document.querySelectorAll(".filter-btn");
 
+//demostrting the drag and drop functionality
+let resources =JSON.parse(localStorage.getItem("resources"));
+let currentfilter = "all";
+let searchTerm = "";
